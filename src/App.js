1f5f9ae7b8229c7dc0a043c763784be0b01f4b85
Robin <mehmet.ucar@imediapro.co.uk>
2020-08-20 15:43:18 +0100

    users setup manually

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Navbar from './components/layout/Navbar.js';
-import UserItem from './components/users/UserItem.js'
-import './App.css';
+import React from "react";
+import Navbar from "./components/layout/Navbar.js";
+import UserItem from "./components/users/UserItem.js";
+import Users from "./components/users/Users";
+import "./App.css";
 
 class App extends React.Component {
   render() {
-  return (
-    <div className="App">
-      <Navbar />
-      <UserItem />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
   }
 }
 
