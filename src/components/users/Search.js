27b6b25c3ea06b36ea654b0c6c84e3a067a4ce68
Robin <mehmet.ucar@imediapro.co.uk>
2020-08-21 20:06:55 +0100

    clear users method added and clear button displays conditionally

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,35 +1,50 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 export default class Search extends Component {
   state = {
-    text: ''
+    text: "",
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
-  }
-  onSubmit = event => {
-
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
+  onSubmit = (event) => {
     event.preventDefault();
-    this.props.searchUsers(this.state.text)
-    this.setState({text: ''});
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
 
-  }
-
-  onChange = event => {
-    this.setState({ text: event.target.value})
-
-  }
-
+  onChange = (event) => {
+    this.setState({ text: event.target.value });
+  };
 
   render() {
+
+    const {showClear, clearUsers} = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
-          <input type="text" name="text" placeholder="Search User..." value={this.state.text} onChange={this.onChange} />
+          <input
+            type="text"
+            name="text"
+            placeholder="Search User..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input type="submit" className="btn btn-dark btn-block" />
         </form>
+        {showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
